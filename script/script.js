1f43c16ccfd39d0f693cc72767e2f893dac2a597
Franco <calderonfranco307@gmail.com>
2024-05-09 22:43:04 -0300

    creando repo local

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+var swiper = new Swiper(".mySwiper", {
+    
+    slidesPerView: 1,
+    SpaceBetween: 30,
+    grabCursor:true,
+    loop:true,
+    breakpoints : {
+        911: {
+            slidesPerView:4
+        }
+    }
+
+}); 
